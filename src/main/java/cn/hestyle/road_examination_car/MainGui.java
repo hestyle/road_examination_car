67c65e693f00fdda337c43639efe8c10ecf3b69a
hestyle <hestyle@qq.com>
2020-12-11 11:52:28 +0800

    first commit

---
+++ src/main/java/cn/hestyle/road_examination_car/MainGui.java
@@ -0,0 +1,46 @@
+/*
+ * Created by JFormDesigner on Fri Dec 11 11:48:06 CST 2020
+ */
+
+package cn.hestyle.road_examination_car;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author hestyle
+ */
+public class MainGui extends JFrame {
+    public MainGui() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        {
+            // compute preferred size
+            Dimension preferredSize = new Dimension();
+            for(int i = 0; i < contentPane.getComponentCount(); i++) {
+                Rectangle bounds = contentPane.getComponent(i).getBounds();
+                preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+                preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+            }
+            Insets insets = contentPane.getInsets();
+            preferredSize.width += insets.right;
+            preferredSize.height += insets.bottom;
+            contentPane.setMinimumSize(preferredSize);
+            contentPane.setPreferredSize(preferredSize);
+        }
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
