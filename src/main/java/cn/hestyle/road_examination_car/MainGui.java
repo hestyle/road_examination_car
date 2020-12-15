4fe758f31007a90f7767699435da525d0a8a7792
fantasy995 <1642063542@qq.com>
2020-12-15 21:57:37 +0800

    feature:使用退出标志结束连接创建线程serverSocketHandler

--- src/main/java/cn/hestyle/road_examination_car/MainGui.java
+++ src/main/java/cn/hestyle/road_examination_car/MainGui.java
@@ -108,7 +108,7 @@ public class MainGui extends JFrame {
     private void stopTcpButtonActionPerformed(ActionEvent e) {
         // 关闭tcp服务器
         try {
-            this.serverSocketHandler.stop();
+            this.serverSocketHandler.exit = true;
             this.server.close();
         } catch (Exception exception) {
             exception.printStackTrace();
@@ -232,6 +232,7 @@ public class MainGui extends JFrame {
 
 
     class ServerSocketHandler extends Thread{
+        public volatile boolean exit = false;
         private ServerSocket serverSocket;
         private JFrame currentFrame;
         public ServerSocketHandler(ServerSocket serverSocket, JFrame currentFrame){
@@ -241,7 +242,7 @@ public class MainGui extends JFrame {
 
 
         public void run(){
-            while (true){
+            while (!exit){
                 Socket socket = null;
                 System.out.println("等待一个连接");
                 try {
